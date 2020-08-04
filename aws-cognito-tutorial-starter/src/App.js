dadcbf71eef7eccc2929ddbe043f5396e2615e75
Pramesh Bajracharya <pe.messh@gmail.com>
2020-08-04 16:29:28 +0530

    Learning AWS cognito.

---
+++ aws-cognito-tutorial-starter/src/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './App.css';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Products from './components/Products';
+import ProductAdmin from './components/ProductAdmin';
+import LogIn from './components/auth/LogIn';
+import Register from './components/auth/Register';
+import ForgotPassword from './components/auth/ForgotPassword';
+import ForgotPasswordVerification from './components/auth/ForgotPasswordVerification';
+import ChangePassword from './components/auth/ChangePassword';
+import ChangePasswordConfirm from './components/auth/ChangePasswordConfirm';
+import Welcome from './components/auth/Welcome';
+import Footer from './components/Footer';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faEdit } from '@fortawesome/free-solid-svg-icons';
+library.add(faEdit);
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <div>
+            <Navbar />
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/products" component={Products} />
+              <Route exact path="/admin" component={ProductAdmin} />
+              <Route exact path="/login" component={LogIn} />
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/forgotpassword" component={ForgotPassword} />
+              <Route exact path="/forgotpasswordverification" component={ForgotPasswordVerification} />
+              <Route exact path="/changepassword" component={ChangePassword} />
+              <Route exact path="/changepasswordconfirmation" component={ChangePasswordConfirm} />
+              <Route exact path="/welcome" component={Welcome} />
+            </Switch>
+            <Footer />
+          </div>
+        </Router>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ aws-cognito-tutorial-starter/src/components/auth/ChangePassword.js
@@ -0,0 +1,121 @@
+import React, { Component } from 'react';
+import FormErrors from "../FormErrors";
+import Validate from "../utility/FormValidation";
+
+class ChangePassword extends Component {
+  state = {
+    oldpassword: "",
+    newpassword: "",
+    confirmpassword: "",
+    errors: {
+      cognito: null,
+      blankfield: false,
+      passwordmatch: false
+    }
+  }
+
+  clearErrorState = () => {
+    this.setState({
+      errors: {
+        cognito: null,
+        blankfield: false,
+        passwordmatch: false
+      }
+    });
+  }
+
+  handleSubmit = async event => {
+    event.preventDefault();
+
+    // Form validation
+    this.clearErrorState();
+    const error = Validate(event, this.state);
+    if (error) {
+      this.setState({
+        errors: { ...this.state.errors, ...error }
+      });
+    }
+
+    // AWS Cognito integration here
+  };
+
+  onInputChange = event => {
+    this.setState({
+      [event.target.id]: event.target.value
+    });
+    document.getElementById(event.target.id).classList.remove("is-danger");
+  }
+
+  render() {
+    return (
+      <section className="section auth">
+        <div className="container">
+          <h1>Change Password</h1>
+          <FormErrors formerrors={this.state.errors} />
+
+          <form onSubmit={this.handleSubmit}>
+            <div className="field">
+              <p className="control has-icons-left">
+                <input 
+                  className="input" 
+                  type="password"
+                  id="oldpassword"
+                  placeholder="Old password"
+                  value={this.state.oldpassword}
+                  onChange={this.onInputChange}
+                />
+                <span className="icon is-small is-left">
+                  <i className="fas fa-lock"></i>
+                </span>
+              </p>
+            </div>
+            <div className="field">
+              <p className="control has-icons-left">
+                <input
+                  className="input"
+                  type="password"
+                  id="newpassword"
+                  placeholder="New password"
+                  value={this.state.newpassword}
+                  onChange={this.onInputChange}
+                />
+                <span className="icon is-small is-left">
+                  <i className="fas fa-lock"></i>
+                </span>
+              </p>
+            </div>
+            <div className="field">
+              <p className="control has-icons-left">
+                <input
+                  className="input"
+                  type="password"
+                  id="confirmpassword"
+                  placeholder="Confirm password"
+                  value={this.state.confirmpassword}
+                  onChange={this.onInputChange}
+                />
+                <span className="icon is-small is-left">
+                  <i className="fas fa-lock"></i>
+                </span>
+              </p>
+            </div>
+            <div className="field">
+              <p className="control">
+                <a href="/forgotpassword">Forgot password?</a>
+              </p>
+            </div>
+            <div className="field">
+              <p className="control">
+                <button className="button is-success">
+                  Change password
+                </button>
+              </p>
+            </div>
+          </form>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default ChangePassword;
