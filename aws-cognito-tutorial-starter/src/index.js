5f7655be0c19566db4e749781bc53afaed476a57
Pramesh Bajracharya <pe.messh@gmail.com>
2020-08-04 16:35:43 +0530

    Cognito config in react + amplify done.

--- aws-cognito-tutorial-starter/src/index.js
+++ aws-cognito-tutorial-starter/src/index.js
@@ -5,6 +5,18 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import Amplify from 'aws-amplify';
+import config from './config.json';
+
+Amplify.configure({
+    Auth: {
+        mandatorySignIn: true,
+        region: config.cognito.REGION,
+        userPoolId: config.cognito.USER_POOL_ID,
+        userPoolWebClientId: config.cognito.APP_CLIENT_ID
+    }
+})
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
