26f212f960b77932691cb617c2fa8d5a59734731
Pramesh Bajracharya <pe.messh@gmail.com>
2020-12-08 10:48:20 +0545

    Download URL done.

--- yt-bazra-download/backend/apis/handler.py
+++ yt-bazra-download/backend/apis/handler.py
@@ -2,23 +2,36 @@ import io
 import json
 import boto3
 import traceback
+
 from pytube import YouTube, Stream
 
 
 def downloadThisUrl(event, context):
+    '''
+    This is the lamba function for POST method which takes in a video URL and downloads it to s3.
+    '''
     try:
         s3 = boto3.client('s3')
-        youtubeVideo = YouTube(
-            'https://www.youtube.com/watch?v=uFjS8ZMyvNA&ab_channel=MatthewMoniz'
-        )
+
+        requestBody = json.loads(event["body"])
+        videoUrl = requestBody["videoUrl"]
+
+        youtubeVideo = YouTube(videoUrl)
         videoStream = youtubeVideo.streams.get_highest_resolution()
         videoFileName = videoStream.default_filename
         videoStream.download(f"/tmp/")
         s3.upload_file(f"/tmp/{videoFileName}", "yt-bazra-download-content",
                        videoFileName)
-        body = {"message": "Got input", "input": event}
+        videoDownloadUrl = s3.generate_presigned_url('get_object',
+                                        Params={
+                                            'Bucket':
+                                            "yt-bazra-download-content",
+                                            'Key': videoFileName
+                                        },
+                                        ExpiresIn=300)
+        body = {"message": "Success.", "videoDownloadUrl": videoDownloadUrl}
         response = {"statusCode": 200, "body": json.dumps(body)}
         return response
     except Exception as e:
         print(traceback.format_exc())
-        return {"statusCode": 200, "body": traceback.format_exc()}
+        return {"statusCode": 500, "body": traceback.format_exc()}
